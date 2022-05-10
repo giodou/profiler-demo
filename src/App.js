704a1493afe9685f162231f132354c22320550f5
Giovane Timm <giovane.timm@sambatech.com.br>
2022-05-10 20:17:09 -0300

    React profile API example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useEffect, useState, Profiler } from "react";
+// component
+import Languages from "./components/Languages";
 function App() {
+  let topLang = ["JavaScript", "Python", "Rust", "TypeScript", "C++", "C"]
+  const [languages, setLanaguages] = useState([]);
+  const [count, setCount] = useState(0);
+  const dummyAPICall = () => setTimeout(() => setLanaguages(topLang), 5000)
+
+  useEffect(() => {
+    dummyAPICall();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <header className='App-header'>
+        {languages.length === 0 && <h2>Welcome!</h2>}
+        {languages.length ? (
+          <Profiler
+              id="language"
+              onRender={(id, phase, actualDuration) =>
+              console.log({ id, phase, actualDuration })}
+            >
+            <Languages languages={languages} />
+          </Profiler>
+        ) : (
+          <div className='text-warning text-center m-4'>
+            Loading languages...
+          </div>
+        )}
+        <hr />
+        <h4>{count}</h4>
+        <button
+          className='mb-3 btn btn-primary'
+          onClick={() => setCount((count) => count + 1)}
         >
-          Learn React
-        </a>
+          Add count
+        </button>
       </header>
     </div>
   );
